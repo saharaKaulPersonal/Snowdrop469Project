81c9825ff92cd11246dd20d4691a0d095e977b5f
Kelsey <kelseypattison1@gmail.com>
2026-03-11 14:01:05 -0600

    removed unneeded function

--- scoring_functions.py
+++ scoring_functions.py
@@ -61,7 +61,7 @@ def APS(model_output, true_label, regularized=False, lambda_r=0.05):
 
         if regularized:
             aps_score += i*lambda_r
-            
+
         scores.append(float(aps_score))
 
     return torch.tensor(scores)
@@ -85,43 +85,4 @@ def margin(model_output, true_label):
     max_val = mo.max(dim=1).values
 
     return max_val - true_values
-
-def RAPS(model_output, true_label, l):
-    """
-    Compute the Average Precision Score (APS)
-
-    Args:
-        model_output (torch.Tensor): (batch, K)
-        true_label (torch.Tensor): (batch,)
-    Returns:
-        torch.Tensor: (batch,)
-    """
-
-    batch_size = model_output.shape[0]
-    scores = []
-
-    for b in range(batch_size):
-
-        probs = model_output[b]
-        y = true_label[b].item()
-
-        # create a one hot vector to keep track of where the true label went
-        one_hot = np.zeros_like(probs)
-        one_hot[y] = 1
-
-        # sort both lists in the same way
-        pairs = list(zip(probs, one_hot))
-        pairs = sorted(pairs, key=lambda x: x[0], reverse=True)
-
-        # calculate score
-        aps_score = pairs[0][0]
-        i = 0
-        while pairs[i][1] != 1:
-            i += 1
-            aps_score += pairs[i][0]
-
-        scores.append(float(aps_score)+(l*i))
-
-    return torch.tensor(scores)
-
 
